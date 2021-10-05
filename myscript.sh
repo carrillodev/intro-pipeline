ca0fa2f98a5c5de756328408357cd91c75ee7568
L. Felipe Carrillo Alvarado <felipe.carrillo1@hotmail.com>
2021-10-05 12:13:16 -0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -8,17 +8,17 @@ add_user() {
   shift; shift;
   #Having shifted twice, the rest is now comments...
   COMMENTS=$@
-  echo "Adding user $USER ..."
-  echo useradd -c "$COMMENTS" $USER
-  echo password $USER $PASSWORD
-  ECHO "Added user $USER ($COMMENTS) with pass $PASSWORD
+  echo "Adding user $USER ...";
+  echo useradd -c "$COMMENTS" $USER;
+  echo password $USER $PASSWORD;
+  echo "Added user $USER ($COMMENTS) with pass $PASSWORD";
 }
 
 ###
 # cuerpo principal del script comienza aqui
-echo "Starting script ..."
+echo "Starting script ...";
 add_user bob sanchez Bob el constructor
 add_user felipe badpassword Felipe Carrillo
 add_user pedro bestpassword Pedro Carmona
-echo "el final de nuestro script ..."
-echo "GL HF"
+echo "el final de nuestro script ...";
+echo "GL HF";
